f938577af45359f6a0797d15bece97d2f3d49de6
Pheeeeenom <emdazer@gmail.com>
2019-02-13 06:31:13 -0500

    Converted to C#
    Changed lang for compatibility and speed

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(163, 257);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(114, 51);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "STERILIZE";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(12, 22);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(137, 17);
+            this.lbl.TabIndex = 1;
+            this.lbl.Text = "Name with extension";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(177, 19);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(145, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(410, 320);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.btn);
+            this.Name = "Form1";
+            this.Text = "STERILIZE ME";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            String name = txtName.Text;
+             byte[] magic = { 0x49, 0x47, 0x47, 0x2D };
+            byte[] temp = new byte[4];
+             byte[] zero =  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+            FileStream fs = new FileStream(name, FileMode.OpenOrCreate);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bool a;
+            while (fs.Read(temp, 0, temp.Length) > 0)
+            {
+                a = temp.SequenceEqual(magic);
+                if (a)
+                {
+                    fs.Seek(fs.Position - 4, SeekOrigin.Begin);
+                    
+                    bw.Write(zero);
+                    break;
+                }
+              
+            }
+           bw.Flush();
+                fs.Flush();
+                bw.Close();
+                fs.Close();
+                
+            MessageBox.Show("Done");
+
+        }
+    }
+}
