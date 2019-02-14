beb8300906758c9f40a8bb6a152ee9cf195244f1
Phx <18449637+NetLogon@users.noreply.github.com>
2019-02-14 12:47:50 +0200

    Open File Dialog

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -30,57 +30,36 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btn = new System.Windows.Forms.Button();
-            this.lbl = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btn
             // 
-            this.btn.Location = new System.Drawing.Point(163, 257);
+            this.btn.Location = new System.Drawing.Point(122, 209);
+            this.btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn.Name = "btn";
-            this.btn.Size = new System.Drawing.Size(114, 51);
+            this.btn.Size = new System.Drawing.Size(86, 41);
             this.btn.TabIndex = 0;
             this.btn.Text = "STERILIZE";
             this.btn.UseVisualStyleBackColor = true;
             this.btn.Click += new System.EventHandler(this.btn_Click);
             // 
-            // lbl
-            // 
-            this.lbl.AutoSize = true;
-            this.lbl.Location = new System.Drawing.Point(12, 22);
-            this.lbl.Name = "lbl";
-            this.lbl.Size = new System.Drawing.Size(137, 17);
-            this.lbl.TabIndex = 1;
-            this.lbl.Text = "Name with extension";
-            // 
-            // txtName
-            // 
-            this.txtName.Location = new System.Drawing.Point(177, 19);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(145, 22);
-            this.txtName.TabIndex = 2;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(410, 320);
-            this.Controls.Add(this.txtName);
-            this.Controls.Add(this.lbl);
+            this.ClientSize = new System.Drawing.Size(308, 260);
             this.Controls.Add(this.btn);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "STERILIZE ME";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn;
-        private System.Windows.Forms.Label lbl;
-        private System.Windows.Forms.TextBox txtName;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,32 +19,38 @@ namespace WindowsFormsApp1
 
         private void btn_Click(object sender, EventArgs e)
         {
-            String name = txtName.Text;
-             byte[] magic = { 0x49, 0x47, 0x47, 0x2D };
-            byte[] temp = new byte[4];
-             byte[] zero =  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "|*.exe";
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                
+                byte[] magic = { 0x49, 0x47, 0x47, 0x2D };
+                byte[] temp = new byte[4];
+                byte[] zero =  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-            FileStream fs = new FileStream(name, FileMode.OpenOrCreate);
-            BinaryWriter bw = new BinaryWriter(fs);
-            bool a;
-            while (fs.Read(temp, 0, temp.Length) > 0)
-            {
-                a = temp.SequenceEqual(magic);
-                if (a)
+                FileStream fs = new FileStream(openFileDialog.FileName,FileMode.OpenOrCreate);
+                BinaryWriter bw = new BinaryWriter(fs);
+                bool a;
+                while (fs.Read(temp, 0, temp.Length) > 0)
                 {
-                    fs.Seek(fs.Position - 4, SeekOrigin.Begin);
-                    
-                    bw.Write(zero);
-                    break;
+                    a = temp.SequenceEqual(magic);
+                    if (a)
+                    {
+                        fs.Seek(fs.Position - 4, SeekOrigin.Begin);
+
+                        bw.Write(zero);
+                        break;
+                    }
+
                 }
-              
-            }
-           bw.Flush();
+                bw.Flush();
                 fs.Flush();
                 bw.Close();
                 fs.Close();
-                
+            }
             MessageBox.Show("Done");
 
         }
