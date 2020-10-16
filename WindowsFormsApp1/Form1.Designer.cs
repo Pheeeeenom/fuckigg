f8f39e8729671d4db82c7585f18cc2f3ff82ddfa
Tiago Sa <tiagossa.2000@gmail.com>
2020-10-16 21:26:43 +0100

    Clean code fixes.

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -35,40 +35,42 @@
             // 
             // btn
             // 
-            this.btn.Location = new System.Drawing.Point(147, 259);
-            this.btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn.Location = new System.Drawing.Point(211, 50);
+            this.btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn.Name = "btn";
-            this.btn.Size = new System.Drawing.Size(115, 50);
+            this.btn.Size = new System.Drawing.Size(86, 41);
             this.btn.TabIndex = 0;
             this.btn.Text = "STERILIZE";
             this.btn.UseVisualStyleBackColor = true;
-            this.btn.Click += new System.EventHandler(this.btn_Click);
+            this.btn.Click += new System.EventHandler(this.Btn_Click);
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(125, 34);
+            this.txtName.Location = new System.Drawing.Point(93, 7);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(109, 22);
+            this.txtName.Size = new System.Drawing.Size(204, 20);
             this.txtName.TabIndex = 1;
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(16, 34);
+            this.lblName.Location = new System.Drawing.Point(7, 11);
+            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(103, 17);
+            this.lblName.Size = new System.Drawing.Size(78, 13);
             this.lblName.TabIndex = 2;
             this.lblName.Text = "Name of Game";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(411, 320);
+            this.ClientSize = new System.Drawing.Size(308, 102);
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.txtName);
             this.Controls.Add(this.btn);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "FUCK-IGG";
             this.ResumeLayout(false);
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using Newtonsoft.Json;
+using fuckIGG;
+
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
@@ -18,26 +20,24 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void btn_Click(object sender, EventArgs e)
+        private void Btn_Click(object sender, EventArgs e)
         {
             //assign offset and patches to ints
 
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "|*.exe";
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "|*.exe"
+            };
+
             DialogResult result = openFileDialog.ShowDialog();
 
             if (result == DialogResult.OK)
             {
-
                 string dir = Path.GetDirectoryName(openFileDialog.FileName);
-                string[] garbage = Directory.GetFiles(dir);
 
-                foreach (string file in garbage)
+                foreach (string file in Array.FindAll(Directory.GetFiles(dir), x => x.ContainsIgnoreCase("igg") || x.ContainsIgnoreCase("gamestorrent.co")))
                 {
-                    if (file.Contains("IGG")|| file.Contains("GAMESTORRENT.CO"))
-                    {
-                        File.Delete(file);
-                    }
+                    File.Delete(file);
                 }
 
                 string f = File.ReadAllText("fuckyou.json");
@@ -45,36 +45,27 @@ namespace WindowsFormsApp1
 
                 string i = txtName.Text;
                 //prints json entry data to console
-               
-             
 
-                FileStream fs = new FileStream(openFileDialog.FileName, FileMode.OpenOrCreate);
-                BinaryWriter bw = new BinaryWriter(fs);
-
-                string game_exec = openFileDialog.FileName.Substring(openFileDialog.FileName.LastIndexOf("\\")+1, openFileDialog.FileName.Length - openFileDialog.FileName.LastIndexOf("\\")-1);
-
-                if (jsonObj[i]["Exec_name"] == game_exec)
+                using (FileStream fs = new FileStream(openFileDialog.FileName, FileMode.OpenOrCreate))
                 {
-                 
-                    int offset = Convert.ToInt32(jsonObj[i]["Offset"].ToString(), 16);
-                    int patches = Convert.ToInt32(jsonObj[i]["Patches"].ToString(), 16);
+                    using (BinaryWriter bw = new BinaryWriter(fs))
+                    {
+                        string game_exec = openFileDialog.FileName.Substring(openFileDialog.FileName.LastIndexOf("\\") + 1, openFileDialog.FileName.Length - openFileDialog.FileName.LastIndexOf("\\") - 1);
 
-                    /*set position of stream to offset to apply patches*/
-                    bw.BaseStream.Position = offset;
+                        if (jsonObj[i]["Exec_name"] == game_exec)
+                        {
+                            int offset = Convert.ToInt32(jsonObj[i]["Offset"].ToString(), 16);
+                            int patches = Convert.ToInt32(jsonObj[i]["Patches"].ToString(), 16);
 
-                    //write patch to file
-                    bw.Write((byte)patches);
-                    
+                            /*set position of stream to offset to apply patches*/
+                            bw.BaseStream.Position = offset;
+
+                            //write patch to file
+                            bw.Write((byte)patches);
+                        }
+                    }
                 }
-
-                bw.Flush();
-                fs.Flush();
-                bw.Close();
-                fs.Close();
-              
             }
-            
-
         }
     }
 }
